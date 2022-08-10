7fe90bb107bc666bd4eeb85830e0259bce79ddd7
Jorge Guillén <73417107+itsAmauriDev@users.noreply.github.com>
2022-08-10 00:45:18 -0500

    Add Ejercicio-02

---
+++ Ejercicio-02/index.js
@@ -0,0 +1,12 @@
+const datos = {
+    name: "Jorge Guillén",
+    age: 27,
+    isDev: true,
+    dateOfBirth: new Date(1995, 0, 19),
+    favBook: {
+        title: "1984",
+        author: "George Orwell",
+        date: new Date(1949, 5, 8),
+        url: "https://www.amazon.com.mx/dp/B00BFGQYRQ/"
+    }
+}
